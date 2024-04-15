d0620772ea03b818d666979d4b19917452765f2d
minhvu278 <dominhvu278@gmail.com>
2024-04-15 23:00:53 +0700

    Add layout nav and footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,14 +3,13 @@
 import React from 'react';
 import {CustomButtonProps} from "@/types";
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
     return (
         <button
             disabled={false}
-            type={"button"}
+            type={btnType || "button"}
             className={`custom-btn ${containerStyles}`}
-            onClick={() => {
-            }}
+            onClick={handleClick}
         >
             <span className={`flex-1`}>
                 {title}
